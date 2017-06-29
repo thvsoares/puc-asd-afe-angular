01fd487c20f941f15d5a8541f0f2fbe727d6e95f
Thiago Soares <thiagomullet@gmail.com>
2017-06-29 20:36:29 -0300

    Criação dos componentes

--- projeto/src/app/app.module.ts
+++ projeto/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PainelComponent } from './painel/painel.component';
+import { PesquisaComponent } from './pesquisa/pesquisa.component';
+import { ImagemComponent } from './imagem/imagem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelComponent,
+    PesquisaComponent,
+    ImagemComponent
   ],
   imports: [
     BrowserModule
