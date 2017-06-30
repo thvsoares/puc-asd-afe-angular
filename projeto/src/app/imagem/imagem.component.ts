7d7142cdb6f1c38bbad5e2a97200faeabd0d496b
Thiago Soares <thiagomullet@gmail.com>
2017-06-29 21:21:46 -0300

    Parametrização da imagem

--- projeto/src/app/imagem/imagem.component.ts
+++ projeto/src/app/imagem/imagem.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./imagem.component.css']
 })
 export class ImagemComponent implements OnInit {
-
+  imageUrl = "http://lorempixel.com/600/400/";
   constructor() { }
 
   ngOnInit() {
